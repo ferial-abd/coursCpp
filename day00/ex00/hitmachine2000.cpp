9172847349eb956a95abc8f7f6a580e5c6ceff9e
Ferial <abdelliferial@gmail.com>
2021-09-13 11:59:15 +0200

    commit ex01

---
+++ day00/ex00/hitmachine2000.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+ int main (){
+	 std::cout <<"Moi je m'appelle Lolita \n Lo ou bien Lola, pareil au même Moi je m'appelle 'Lolita' \n Quand je rêve aux loups , c'estr Lola qui saigne [...] \n C'est pas ma faute \n Et quand je donne ma langue aux chats je vois les autres  \n Tous prêts à se jeter sur moi, c'est pas ma faute à moi si j'entends tout autour de moi  \n si j'entends tout autour de moi \n L.O.L.I.T.A, moi 'Lolita";
+
+
+	return 0;
+ }
---
+++ day00/ex01/bonjour.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int bonjour(std::string name){
+
+std::cout << "Bonjour " << name << " comment allez vous ?\n";
+return 0;
+}
+
+
+int main (){
+ 
+	bonjour ("ferial"); 
+	return 0;
+}
+
